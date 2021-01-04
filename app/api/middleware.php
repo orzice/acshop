abebee1565eb4966c75eb38a173e6914c5438569
小涛 <1073519986@qq.com>
2021-01-04 17:33:51 +0800

    push

--- app/api/middleware.php
+++
@@ -1,8 +0,0 @@
-<?php
-// 全局中间件定义文件
-return [
-
-    // Session初始化
-    \think\middleware\SessionInit::class,
-
-];
--- app/common/model/PageNotice.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\common\model;
-use app\common\model\TimeModel;
-
-
-class PageNotice extends TimeModel
-{
-
-}
